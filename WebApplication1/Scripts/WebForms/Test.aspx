1e07a78d45c29a2ec081aed1fa71c04b2903d5a4
shrutujag-cuelogic <shrutuja.ghogre@cuelogic.co.in>
2016-05-13 14:53:35 +0530

    aster branch comit

---
+++ WebApplication1/Scripts/WebForms/Test.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Test.aspx.cs" Inherits="WebApplication1.Scripts.WebForms.Test" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:TextBox ID="txt1" runat="server"></asp:TextBox>
+
+        <asp:TextBox ID="txt2" runat="server"></asp:TextBox>
+
+        <asp:Button ID="Add" runat="server" Text="Button" OnClick="Add_Click" />
+
+        <asp:TextBox ID="txt3" runat="server"></asp:TextBox>
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/Scripts/WebForms/Test.aspx.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1.Scripts.WebForms
+{
+    public partial class Test : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Add_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(txt1.Text);
+            int b = Convert.ToInt32(txt2.Text);
+
+            int c;
+            c = a + b;
+
+            txt3.Text = c.ToString();
+          
+        }
+    }
+}
